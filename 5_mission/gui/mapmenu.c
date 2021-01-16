5f8ed5198f54d72683e7d5d323528ef6dd301f64
Ermiq <tetskatlipokka@yandex.ru>
2021-01-16 14:58:41 +0300

    .

--- 5_mission/gui/mapmenu.c
+++ 5_mission/gui/mapmenu.c
@@ -1,9 +1,11 @@
 modded class MapMenu extends UIScriptedMenu
 {
 	override Widget Init() {
+		
 		layoutRoot = super.Init();
 
 		MapWidget mapWidget = MapWidget.Cast(layoutRoot.FindAnyWidget("Map"));
+		
 		PVEZ_MapMarkersDrawer.LoadPVEZMarkers(mapWidget);
 
 		if (g_Game.pvez_Config.MAP.Show_Player_Marker == true) {
@@ -12,7 +14,7 @@ modded class MapMenu extends UIScriptedMenu
 				label = g_Game.pvez_Config.MAP.Custom_Player_Marker_Text;
 			else
 				label = Widget.TranslateString("#pvez_player_map_label");
-			mapWidget.AddUserMark(GetGame().GetPlayer().GetPosition(), label, COLOR_BLUE, "\\dz\\gear\\navigation\\data\\map_waypoint_ca.paa");
+			mapWidget.AddUserMark(GetGame().GetPlayer().GetPosition(), label, COLOR_BLUE, "\\DZ\\gear\\navigation\\data\\map_waypoint_ca.paa");
 		}
 		
 		return layoutRoot;
